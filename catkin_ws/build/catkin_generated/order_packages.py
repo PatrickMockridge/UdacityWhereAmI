43c5215258ad7acd12949b5ad7d258adb7c1efb8
Patrick Mockridge <patrick@engzig.com>
2018-06-20 19:10:20 +0100

    navigation successful with basic robot

--- catkin_ws/build/catkin_generated/order_packages.py
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/patrick/robotics/UdacityWhereAmI/catkin_ws/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/opt/ros/kinetic".split(';') if "/opt/ros/kinetic" != "" else []
+underlay_workspaces = "/home/patrick/robotics/UdacityWhereAmI/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/patrick/robotics/UdacityWhereAmI/catkin_ws/devel;/opt/ros/kinetic" != "" else []
