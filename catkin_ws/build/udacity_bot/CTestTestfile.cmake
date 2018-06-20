4e70be0f0cfb04bd58ad9cd95a986e8b9e8f3898
Patrick Mockridge <patrick@engzig.com>
2018-06-20 12:32:23 +0100

    first commit, ready for cloud GPU testing

---
+++ catkin_ws/build/udacity_bot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/patrick/robotics/UdacityWhereAmI/catkin_ws/src/udacity_bot
+# Build directory: /home/patrick/robotics/UdacityWhereAmI/catkin_ws/build/udacity_bot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
